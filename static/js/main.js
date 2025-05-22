a99d6a4bade286c089afa673a37436ce69aae4f3
nye3code <jmechaveznet@zoho.com>
2025-05-22 11:14:49 +0800

    ✨ feat: Added Header

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,19 @@
-import '../css/modern-normalize.css';
-import '../css/styles.css';
+// static/js/main.js
+window.loadStylesheets = function() {
+    const stylesheets = [
+        '/static/css/styles.css',
+        `/static/css/components/header.css`,
+        '/static/css/utils.css'
+    ];
+    stylesheets.forEach(url => {
+        if (!document.querySelector(`link[href="${url}"]`)) {
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = url;
+            document.head.appendChild(link);
+            console.log(`Loaded stylesheet: ${url}`);
+        }
+    });
+};
 
+console.log("main.js loaded");
