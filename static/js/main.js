33cc0c4e2ac1fd6e5c2172c746e6aa5a5938e02c
nye3code <jmechaveznet@zoho.com>
2025-05-23 10:26:22 +0800

    ✨ feat: Added Welcome Screen

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,20 @@
 // static/js/main.js
-window.loadStylesheets = function() {
-    const stylesheets = [
-        '/static/css/styles.css',
-        `/static/css/components/header.css`,
-        '/static/css/utils.css'
-    ];
-    stylesheets.forEach(url => {
-        if (!document.querySelector(`link[href="${url}"]`)) {
-            const link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = url;
-            document.head.appendChild(link);
-            console.log(`Loaded stylesheet: ${url}`);
-        }
-    });
+window.loadStylesheets = function () {
+  const stylesheets = [
+    "/static/css/styles.css",
+    "/static/css/components/header.css",
+    "/static/css/components/welcome.css",
+    "/static/css/utils.css",
+  ];
+  stylesheets.forEach((url) => {
+    if (!document.querySelector(`link[href="${url}"]`)) {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = url;
+      document.head.appendChild(link);
+      console.log(`Loaded stylesheet: ${url}`);
+    }
+  });
 };
 
 console.log("main.js loaded");
